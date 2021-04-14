ac769ea8e57f61c2ac10f7ebbe3bbacd61a67a03
JuliaMendes <juliadepaivams@gmail.com>
2021-04-14 19:20:57 -0300

    js in progress

---
+++ script.js
@@ -0,0 +1,20 @@
+const input = document.querySelector('input')
+const btn = document.querySelectorAll('.buttons')
+
+var arraynumbers = [];
+
+function AddNumber(number){
+    input.value = number;
+    console.log(input.value)
+}
+
+
+
+function clearText(event){
+
+
+}
+
+function clearAll(event){
+    
+}
