cbd3dd0bb90d2e8f74bb4cebef6857acb5758c94
JuliaMendes <juliadepaivams@gmail.com>
2021-05-19 17:42:11 -0300

    modifying the JS

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const input = document.querySelector('input')
 
 let arrayNumbers = []
+let firstNumberOfExpression;
+let choice;
 
 function AddNumber(number){
     arrayNumbers.push(number)
@@ -8,26 +10,75 @@ function AddNumber(number){
     console.log(arrayNumbers)
 }
 
-function percent(event){
+function add(){
+    firstNumberOfExpression = parseFloat(arrayNumbers.join(''));
+    input.value = ''
+    arrayNumbers = []
+    choice = '+'
+}
+
+function subtract(){
+    firstNumberOfExpression = parseFloat(arrayNumbers.join(''));
+    input.value = ''
+    arrayNumbers = []
+    choice = '-'
+}
+
+function multiply(){
+    firstNumberOfExpression = parseFloat(arrayNumbers.join(''));
+    input.value = ''
+    arrayNumbers = []
+    choice = '*'
+}
+
+function devide(){
+    firstNumberOfExpression = parseFloat(arrayNumbers.join(''));
+    input.value = ''
+    arrayNumbers = []
+    choice = '/'
+}
+
+function result(){
+    switch(choice) {
+        case '+':
+            [8] + [6,7,9]
+            input.value = firstNumberOfExpression + parseFloat(arrayNumbers.join(''));
+             break;
+        case '-':
+            input.value = firstNumberOfExpression - parseFloat(arrayNumbers.join(''));
+            break;
+        case '*':
+            input.value = firstNumberOfExpression * parseFloat(arrayNumbers.join(''));
+            break;
+        case '/':
+            input.value = firstNumberOfExpression / parseFloat(arrayNumbers.join(''));
+            break;
+      } 
+}
+
+function percent(){
     var percentage =  arrayNumbers.join('') / 100
     input.value = percentage
 }
 
-function negate(event){
+function negate(){
     var negate = arrayNumbers.join('') * -1
+    console.log(arrayNumbers)
     //var negate = Math.abs(arrayNumbers.join(''))
     input.value = negate
-    console.log(negate)
 }
 
-function clearLastNumber(event){
+function clearLastNumber(){
     arrayNumbers.pop()
     input.value = arrayNumbers.join('');
 
 }
 
-function clearAll(event){
+function clearAll(){
     arrayNumbers = []
     input.value = ''
 }
+
+
+
 
